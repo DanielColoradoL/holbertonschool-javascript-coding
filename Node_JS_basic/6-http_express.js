005e3f8c79914bcd07e7842df0f0d72427d4701e
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-29 16:28:05 +1000

    fix(task 6):added module.exports

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-// Serving static files (HTML, CSS, JS)
-app.use(express.static('public'));
 // Defining a route for handling client communication
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -14,3 +12,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
+
+module.exports = app;
