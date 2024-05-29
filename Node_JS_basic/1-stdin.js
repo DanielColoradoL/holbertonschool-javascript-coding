46f6e5cb9870b8510a09d9644424ee3951c42da2
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-29 11:24:03 +1000

    fix(task 1):swapped console.log

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (idata) => {
-  const data = idata.toString().trim();
-  console.log(`Your name is: ${data}`);
-  console.log('This important software is now closing');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
   process.exit();
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
